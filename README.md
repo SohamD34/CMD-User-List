b92ec08c4ae56f27327174e8cfd5fd6f66fd3e75
SohamD34 <deshmukh.2@iitj.ac.in>
2024-05-19 11:48:24 +0530

    'New functionalities'

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # CMD-User-List
 A user list with user details, accessible using command line 
+
+## How to use and access ?
+
+* Navigate to the desired location and clone the repository.
+``` git clone https://www.github.com/SohamD34/CMD-User-List.git ```
+
+* In CMD, navigate to the folder location
+``` cd your_desired_folder/ ```
+
+### To view the information about the webapp
+``` python list.py -h  ```
+
+### Creating a user 
+``` python list.py -a --username='name' --user_id='id' --user_email='email@email.com' ```
+
+### Viewing user list
+``` python list.py -lu ```
+
+### Viewing user emails list
+``` python list.py -le ```
+
+### Viewing user IDs list
+``` python list.py -li ```
+
+### Editing an user information
+``` python list.py -ed --username='new_name' --user_id='new_id' --user_email='new_email' ```
+
+### Deleting a user 
+``` python list.py -r=index ```
--- list.py
+++ list.py
@@ -3,7 +3,7 @@ import os
 
 def create_parser():
     parser = argparse.ArgumentParser(description = "Users List App")
-    parser.add_argument("-a", "--add_user", metavar="", help="Add a new user")
+    parser.add_argument("-a", "--add_user", action="store_true", help="Add a new user")
     parser.add_argument("-lu", "--list_users", action="store_true", help="List all users")
     parser.add_argument("-le", "--list_emails", action="store_true", help="List all emails")
     parser.add_argument("-li", "--list_ids", action="store_true", help="List all IDs")
@@ -11,6 +11,7 @@ def create_parser():
     parser.add_argument("-n", "--username", metavar="", help="Username")
     parser.add_argument("-i", "--user_id", metavar="", help="User ID")
     parser.add_argument("-e", "--user_email", metavar="", help="User Email")
+    parser.add_argument("-ed", "--edit_user", action="store_true", help="Edit a user by index")
     return parser
 
 
@@ -63,14 +64,16 @@ def remove_user(index):
                     file.write(user)
 
     if os.path.exists("emails.txt"):
-        emails = file.readlines()
+        with open("emails.txt", "r") as file:
+            emails = file.readlines()
         with open("emails.txt", "w") as file:
             for i, email in enumerate(emails, start=1):
                 if i != index:
                     file.write(email)
 
     if os.path.exists("ids.txt"):
-        ids = file.readlines()
+        with open("ids.txt", "r") as file:
+            ids = file.readlines()
         with open("ids.txt", "w") as file:
             for i, id in enumerate(ids, start=1):
                 if i != index:
@@ -79,37 +82,41 @@ def remove_user(index):
     else:
         print("No users found.")
 
-def add(num1, num2):
-    return num1 + num2
 
-def subtract(num1, num2):
-    return num1 - num2
+def edit_user(index, username, user_id, user_email):
+    if os.path.exists("users.txt"):
+        with open("users.txt", "r") as file:
+            users = file.readlines()
+        with open("users.txt", "w") as file:
+            for i, user in enumerate(users, start=1):
+                if i != index:
+                    file.write(user)
+                else:
+                    file.write(username + "\n")
 
-def multiply(num1, num2):
-    return num1 * num2
+    if os.path.exists("emails.txt"):
+        with open("emails.txt", "r") as file:
+            emails = file.readlines()
+        with open("emails.txt", "w") as file:
+            for i, email in enumerate(emails, start=1):
+                if i != index:
+                    file.write(email)
+                else:
+                    file.write(user_email + "\n")
 
-def divide(num1, num2):
-    return num1 / num2
+    if os.path.exists("ids.txt"):
+        with open("ids.txt", "r") as file:
+            ids = file.readlines()
+        with open("ids.txt", "w") as file:
+            for i, id in enumerate(ids, start=1):
+                if i != index:
+                    file.write(id)
+                else:
+                    file.write(user_id + "\n")
+        print("User edited successfully.")
+    else:
+        print("No users found.")
 
-def perform_task():
-    print("Available tasks -")
-    print("1. Add two numbers")
-    print("2. Subtract two numbers")
-    print("3. Multiply two numbers")
-    print("4. Divide two numbers")
-
-    task = int(input("Enter the task number: "))
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-
-    if task == 1:
-        print(add(num1 + num2))
-    elif task == 2:
-        print(subtract(num1, num2))
-    elif task == 3:
-        print(multiply(num1, num2))
-    elif task == 4:
-        print(divide(num1, num2))
 
 def main():
     parser = create_parser()
